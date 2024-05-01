80d791ff9aec48dcd5f5dd08e25ddf61719346a3
Kgotso Phiri <kgotsokg12@gmail.com>
2024-05-01 15:46:14 +0200

    feat: set up get endpoint in the backend

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // For parsing Json
+
+// Make animals
+import Chance from 'chance';
+
+
+const chance = new Chance();
+
+const animals = [...Array(250).keys()].map(id => {
+    return {
+        id,
+        type: chance.animal(),
+        age: chance.age(),
+        name:chance.name(),
+    }
+})
+
+// Endpoints to search for animals
+app.get('/', (req, res) => {
+    const q = req.query.q?.toLowerCase() || '';
+    const results = animals.filter(animal => animal.type.toLowerCase().includes(q))
+
+    res.send(results)
+
+});
+
+app.listen(8080, () => console.log('Listening on port: http//localhost:8080'));
+
+
