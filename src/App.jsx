82e63b20b69af918a2c3a1ec286f66009e1e24fa
Kgotso Phiri <kgotsokg12@gmail.com>
2024-05-01 16:37:38 +0200

    feat: add search functionality on the front eend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import './App.css'
+import { useState } from 'react'
+
+function Animal({ type, age, name }) {
+  return (
+    <li>
+      <strong>{name}</strong> is a {type} and is {age} years old.
+    </li>
+  )
+}
 
 function App() {
+  const [animals, setAnimals] = useState([])
+
+  const search = async (q) => {
+    const response = await fetch('http://localhost:8080?' + new URLSearchParams({ q }))
+
+    const data = await response.json()
+    setAnimals(data)
+  }
 
   return (
     <>
-    <p>Hi this is the app</p>
+      <h1>Animal Farm</h1>
+      {/* Add Form */}
+
+      <input type="text" placeholder='Search Animal' onChange={(e) => search(e.target.value)} />
+
+
+      {/* Display results : Render the response got form the call*/}
+      <ul>
+        {animals.length === 0 && 'No animals Found'}
+
+        {animals.map((animal) => (
+          <Animal type={animal.type} age={animal.age} name={animal.name} key={animal.id} />
+        ))}
+      </ul>
+
     </>
   )
 }
